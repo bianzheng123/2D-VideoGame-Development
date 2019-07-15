371f1f2d5e2448896bbfe57a9ead6b2dfef5516b
Caveman Bian <51192378+bianzheng123@users.noreply.github.com>
2019-07-15 17:10:35 +0800

    Can Upload the background

--- Final_Game_Project/public_html/src/MyGame/Objects/MapManager.js
+++ Final_Game_Project/public_html/src/MyGame/Objects/MapManager.js
@@ -1,7 +1,33 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* File: DyePack.js 
+ *
+ * Creates and initializes a simple DyePack
  */
 
+/*jslint node: true, vars: true */
+/*global gEngine: false, GameObject: false, SpriteRenderable: false */
+/* find out more about jslint: http://www.jslint.com/help.html */
 
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
+
+function MapManager(spriteTexture) {
+    this.kRefWidth = 80;
+    this.kRefHeight = 130;
+    this.kDelta = 2;
+    this.kSlowDelta = 0.1;
+    
+    this.available=true;
+    this.mPlayer = new SpriteRenderable(spriteTexture);
+    this.mPlayer.setColor([1, 1, 1, 0.1]);
+    this.mDyePack.getXform().setPosition(35, 50);
+    this.mPlayer.getXform().setSize(this.kRefWidth / 50, this.kRefHeight / 50);
+    this.mPlayer.setElementPixelPositions(510, 595, 23, 153);
+    
+    GameObject.call(this, this.mPlayer);
+}
+gEngine.Core.inheritPrototype(MapManager, GameObject);
+
+MapManager.prototype.update = function () {
+    
+    
+    
+};
