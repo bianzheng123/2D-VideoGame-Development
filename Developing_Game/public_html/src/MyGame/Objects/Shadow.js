f3a14188e5ea76d817a36cf18f0c6c11e6b69105
陈宇恒(Chen Yuheng) <pass@chenyuheng.cn>
2019-07-17 17:45:48 +0800

    update (new) Shadow.js

---
+++ Developing_Game/public_html/src/MyGame/Objects/Shadow.js
@@ -0,0 +1,18 @@
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
+
+function Shadow(spriteTexture,xform) {
+    this.kTag = "Shadow";
+    
+    this.mShadow = new SpriteRenderable(spriteTexture);
+    this.mShadow.setColor([0.5, 0.5, 0.5, 0.5]);
+    this.mShadow.getXform().setPosition(xform[0],xform[1]);
+    this.mShadow.getXform().setSize(xform[2],xform[3]);
+    this.mShadow.setElementPixelPositions(510, 595, 23, 153);
+
+    GameObject.call(this, this.mShadow);
+}
+gEngine.Core.inheritPrototype(Shadow, GameObject);
+
+Shadow.prototype.update = function (originalX,originalY) {
+    this.mShadow.getXform().setPosition(originalX,originalY);
+};
