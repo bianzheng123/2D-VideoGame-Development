d5e6c925257f498feb4d3ccbff6bdf2b4dc72726
Caveman Bian <51192378+bianzheng123@users.noreply.github.com>
2019-07-23 19:21:40 +0800

    Update EndlessPlayingScene.js

--- Developing_Game/public_html/src/MyGame/EndlessPlayingScene.js
+++ Developing_Game/public_html/src/MyGame/EndlessPlayingScene.js
@@ -198,6 +198,8 @@ EndlessPlayingScene.prototype.update = function () {
         this._countTime();
         if(this.mPlayer.accumulateValue>0.15*60*10){//触发彩蛋
             this.mKunkun.play=true;
+            gEngine.AudioClips.stopBackgroundAudio();
+            gEngine.AudioClips.playJiNiTaiMeiAudio(this.kJinitaimei);
             this.mKunkun.mKunkun.getXform().setPosition(this.mPlayer.getXform().getXPos(),this.mPlayer.getXform().getYPos());
             this.mPlayer.accumulateValue=0;
             this.mPlayer.setBack();
@@ -205,14 +207,20 @@ EndlessPlayingScene.prototype.update = function () {
         if(this.mKunkun.play){
             this.mPlayer.setBack();
             this.mPlayer.accumulateValue=0;
+            gEngine.AudioClips.setCueVolume(0);
             if(this.mKunkun.timecount>60){
                 if(gEngine.Input.isAnyKeyClicked()){//结束彩蛋
                     this.mKunkun.play=false;
                     this.mKunkun.timecount=0;
                 }
             }
+            this.mKunkun.update();
+            if(!this.mKunkun.play){
+                gEngine.AudioClips.setCueVolume(30);
+                    gEngine.AudioClips.stopJiNiTaiMeiAudio();
+                    gEngine.AudioClips.playBackgroundAudio(this.kPlaySceneBgm);
+            }
         }
-        this.mKunkun.update();
         
         this.mIceCreamManager.update(this.mMapManager);
         this.mMapManager.update();
